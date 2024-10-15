22b9217f569b2d82cdbd09a3cf5365cd6de2fe8b
surajsahoo329bbsr <surajsahoo329bbsr@gmail.com>
2024-10-15 12:02:29 +0530

    Revert "Adding working module of Authentication Module, Added SQL Script for starting the Application"
    This reverts commit ddee409edee785b7522d5c53d8cd93c5257d18a5.

--- authentication-service/src/main/java/com/hotifi/web/controller/AuthenticationController.java
+++ authentication-service/src/main/java/com/hotifi/web/controller/AuthenticationController.java
@@ -18,7 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import com.hotifi.exception.errors.ErrorResponse;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
